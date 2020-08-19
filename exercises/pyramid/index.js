1b99b75cb1909b49defb323c7b51301b22eef540
Jude Okagu <juud0010@gmail.com>
2020-08-19 18:43:08 +0200

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,7 +14,38 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// const pyramid = (n) => {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     level = '';
+
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// };
+
+const pyramid = (n, row = 0, level = '') => {
+  if (row === n) return;
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add += '#';
+  } else {
+    add += ' ';
+  }
+  pyramid(n, row, level + add);
+};
 
 // const pyramid = (n) => {
 //   return;
