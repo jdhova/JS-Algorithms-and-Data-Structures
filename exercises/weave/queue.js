ec42a01e818c1b196d2c8365964cf1ad156091c1
Jude Okagu <juud0010@gmail.com>
2020-08-23 23:26:19 +0200

    fix

--- exercises/weave/queue.js
+++ exercises/weave/queue.js
@@ -4,19 +4,19 @@
 // one to be returned) from the queue *without*
 // removing it.
 
-// class Queue {
-//   constructor() {
-//     this.data = [];
-//   }
-//   add(record) {
-//     this.data.unshift(record);
-//   }
-//   remove() {
-//     return this.data.pop();
-//   }
-//   peek() {
-//     return this.data[this.data.length - 1];
-//   }
-// }
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+  add(record) {
+    this.data.unshift(record);
+  }
+  remove() {
+    return this.data.pop();
+  }
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+}
 
 module.exports = Queue;
