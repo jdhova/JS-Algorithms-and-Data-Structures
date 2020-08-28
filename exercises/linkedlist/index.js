627af53b640b051539e4d24ff147ac1796200928
Jude Okagu <juud0010@gmail.com>
2020-08-28 17:44:47 +0200

    linked-add

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -6,4 +6,27 @@ class Node {}
 
 class LinkedList {}
 
+/// Linked List Add
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    // this.tail = null
+    this.size = 0;
+  }
+  addFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+}
+
+const jj = new LinkedList();
+jj.addFirst(100);
+jj.addFirst(200);
+
 module.exports = { Node, LinkedList };
