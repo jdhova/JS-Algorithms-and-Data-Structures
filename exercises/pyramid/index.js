a117cd18248dda64ae409fd0f11ebe36d663c9f5
Jude Okagu <juud0010@gmail.com>
2020-09-06 23:33:00 +0200

    f

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -30,22 +30,22 @@
 //   }
 // };
 
-// const pyramid = (n, row = 0, level = '') => {
-//   if (row === n) return;
+const pyramid = (n, row = 0, level = '') => {
+  if (row === n) return;
 
-//   if (level.length === 2 * n - 1) {
-//     console.log(level);
-//     return pyramid(n, row + 1);
-//   }
-//   const midpoint = Math.floor((2 * n - 1) / 2);
-//   let add;
-//   if (midpoint - row <= level.length && midpoint + row >= level.length) {
-//     add = '#';
-//   } else {
-//     add = ' ';
-//   }
-//   pyramid(n, row, level + add);
-// };
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
+};
 
 // const pyramid = (n) => {
 //   return;
