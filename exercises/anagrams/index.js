f83db225e70bb9973be1aee170980ad7a534b2c6
Jude Okagu <juud0010@gmail.com>
2021-09-11 12:42:44 -0400

    fix

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -20,20 +20,17 @@
 //   return str.replace(/[ˆ\w]/g, '').toLowerCase().split('').sort().join('');
 // };
 
-const anagrams = (stringA, stringB) => {
-  return cleanStr(stringA) === cleanStr(stringB);
-};
-
-const cleanStr = (str) => {
-  return str.replace(/[ˆ\w]/g, '').split('').sort().join('');
-};
-
-const anagrams = (stringA, stringB) => {
-  return cleanStr(stringA) === cleanStr(stringB);
-};
-
-const cleanStr = (str) => {
-  return str.replace(/[ˆ\w]/g, '').split('').sort().join('');
-};
+// const anagrams = (stringA, stringB) => {
+//   return cleanStr(stringA) === cleanStr(stringB);
+// };
+// const anagrams = (stringA, stringB) => {
+//   return cleanStr(stringA) === cleanStr(stringB);
+// };
+// const anagrams = (stringA, stringB) => {
+//   return cleanStr(stringA) === cleanStr(stringB);
+// };
+// const anagrams = (stringA, stringB) => {
+//   return cleanStr(stringA) === cleanStr(stringB);
+// };
 
 module.exports = anagrams;
